984306c5a1b10b61743bcd8d79b4e7ff67906991
Rj <rikrdo.ortega@gmail.com>
2019-06-06 18:19:26 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,7 +1,11 @@
+# def count_elements(array)
+#   array.each_with_object ({}) do |element, hash|
+#     hash[element] == nil ? hash[element] = 1 : hash[element] += 1
+#   end
+# end
+
 def count_elements(array)
-  array.each_with_object ({}) do |element, hash|
-    hash[element] == nil ? hash[element] = 1 : hash[element] += 1
+  array.each_with_object (Hash.new(0)) do |element, hash|
+    hash[element] += 1
   end
 end
- 
-
